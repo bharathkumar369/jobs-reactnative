1843670181133a9a0a1568ad6655b7916b71ef1f
bharathkumar369 <bharathpathapati@gmail.com>
2024-03-24 12:17:25 +0530

    update

--- app/index.js
+++ app/index.js
@@ -1,42 +1,46 @@
 import { useState } from "react";
-import { StyleSheet, Text, View, ScrollView,SafeAreaView } from "react-native";
-import { Stack,useRouter } from "expo-router"; 
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
 
-import { COLORS,icons,images,SIZES } from "../constants";
-import { Nearbyjobs,Popularjobs,ScreenHeaderBtn,Welcome} from "../components"
-
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 export default function Page() {
   const router = useRouter();
-  const [searchTerm,setSearchTerm] = useState("")
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <SafeAreaView style={{flex:1, backgroundColor:COLORS.lightWhite}}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
         options={{
-          headerStyle:{backgroundColor:COLORS.lightWhite},
-          headerShadowVisible:false,
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
           headerLeft: () => (
-            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/>
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
           ),
           headerRight: () => (
-            <ScreenHeaderBtn iconUrl= {images.profile} dimension="100%" />
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
           ),
-          headerTitle: ""
+          headerTitle: "",
         }}
       />
 
       <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={{ flex:1,padding:SIZES.medium}}>
-          <Welcome 
-             searchTerm={searchTerm}
-             setSearchTerm = {setSearchTerm}
-             handleClick = {() => {
-              if(searchTerm){
-                router.push(`/search/${searchTerm}`)
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              if (searchTerm) {
+                router.push(`/search/${searchTerm}`);
               }
-             }}
+            }}
           />
-          <Popularjobs/>
+          <Popularjobs />
           <Nearbyjobs />
         </View>
       </ScrollView>
